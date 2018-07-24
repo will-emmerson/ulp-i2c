9f3981dd16c44f04b3baba6e328f86a07ba645cd
xlfe <felixb@gmail.com>
2018-07-24 21:43:32 +1000

    trying to convert to bmp280

--- main/component.mk
+++ main/component.mk
@@ -19,13 +19,13 @@ ULP_S_SOURCES = $(addprefix $(COMPONENT_PATH)/ulp/, \
 #    generated ULP export file, $(ULP_APP_NAME).h:
 ULP_EXP_DEP_OBJECTS := ulp_example_main.o
 #
-# 4. Include build rules for ULP program 
+# 4. Include build rules for ULP program
 include $(IDF_PATH)/components/ulp/component_ulp_common.mk
 #
 # Link object files and generate map file
-$(ULP_ELF): $(ULP_OBJECTS) $(ULP_LD_SCRIPT)
-	$(summary) ULP_LD $(patsubst $(PWD)/%,%,$(CURDIR))/$@
-	$(ULP_LD) -o $@ -A elf32-esp32ulp -Map=$(ULP_MAP) -T $(ULP_LD_SCRIPT) $(ULP_OBJECTS)
+#$(ULP_ELF): $(ULP_OBJECTS) $(ULP_LD_SCRIPT)
+#	$(summary) ULP_LD $(patsubst $(PWD)/%,%,$(CURDIR))/$@
+#	$(ULP_LD) -o $@ -A elf32-esp32ulp -Map=$(ULP_MAP) -T $(ULP_LD_SCRIPT) $(ULP_OBJECTS)
 #
 # End of ULP support additions to component makefile.
 #
