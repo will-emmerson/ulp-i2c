98e453dff33230174d68896905815564586f92f9
xlfe <felixb@gmail.com>
2018-07-25 23:05:34 +1000

    temperature working now, fixed msb/lsb bug on read16 in the ulp i2c bit bang code

--- main/ulp_example_main.c
+++ main/ulp_example_main.c
@@ -25,11 +25,65 @@
 
 #include "ulp_main.h"
 
+
+/*
+# OSRS_P = 1 # 16 Bit ultra low power
+# OSRS_P = 2 # 17 Bit low power
+# OSRS_P = 3 # 18 Bit standard resolution
+# OSRS_P = 4 # 19 Bit high resolution
+# OSRS_P = 5 # 20 Bit ultra high resolution
+
+# OSRS_T = 0 # skipped
+# OSRS_T = 1 # 16 Bit
+# OSRS_T = 2 # 17 Bit
+# OSRS_T = 3 # 18 Bit
+# OSRS_T = 4 # 19 Bit
+# OSRS_T = 5 # 20 Bit
+
+# FILTER = 0 #
+# FILTER = 1 #
+# FILTER = 2 #
+# FILTER = 3 #
+# FILTER = 4 #
+# FILTER = 5 #
+# FILTER = 6 #
+# FILTER = 7 #
+
+# standby settings
+# T_SB = 0 # 000 0,5ms
+# T_SB = 1 # 001 62.5 ms
+# T_SB = 2 # 010 125 ms
+# T_SB = 3 # 011 250ms
+# T_SB = 4 # 100 500ms
+# T_SB = 5 # 101 1000ms
+# T_SB = 6 # 110 2000ms
+# T_SB = 7 # 111 4000ms
+
+# power mode
+# POWER_MODE=0 # sleep mode
+# POWER_MODE=1 # forced mode
+# POWER_MODE=2 # forced mode
+# POWER_MODE=3 # normal mode
+
+ */
+
+#define OSRS_P 5
+#define OSRS_T 2
+#define T_SB 7
+#define POWER_MODE 1
+#define FILTER 2
+
+#define CONFIG  ((T_SB <<5) + (FILTER <<2))
+#define CTRL_MEAS ((OSRS_T <<5) + (OSRS_P <<2) + POWER_MODE)
+
+
+
+
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
 
 
-const gpio_num_t gpio_led = GPIO_NUM_2;
+//const gpio_num_t gpio_led = GPIO_NUM_2;
 const gpio_num_t gpio_scl = GPIO_NUM_32;
 const gpio_num_t gpio_sda = GPIO_NUM_33;
 
@@ -39,9 +93,11 @@ const gpio_num_t gpio_sda = GPIO_NUM_33;
 
 static void init_ulp_program()
 {
-    rtc_gpio_init(gpio_led);
-    rtc_gpio_set_direction(gpio_led, RTC_GPIO_MODE_OUTPUT_ONLY);
 
+    printf("ulp_config = %x\n", CONFIG);
+    printf("ulp_ctrl = %x\n", CTRL_MEAS);
+    ulp_reg_config = CONFIG;
+    ulp_reg_ctrl= CTRL_MEAS;
     rtc_gpio_init(gpio_scl);
     rtc_gpio_set_direction(gpio_scl, RTC_GPIO_MODE_INPUT_ONLY);
     rtc_gpio_init(gpio_sda);
@@ -50,7 +106,7 @@ static void init_ulp_program()
     ESP_ERROR_CHECK(ulp_load_binary(0, ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t)));
 
     /* Set ULP wake up period to T = 5 seconds */
-    REG_SET_FIELD(SENS_ULP_CP_SLEEP_CYC0_REG, SENS_SLEEP_CYCLES_S0, 150000);
+    REG_SET_FIELD(SENS_ULP_CP_SLEEP_CYC0_REG, SENS_SLEEP_CYCLES_S0, 5*RTC_1S_SLEEP);
 
 }
 
@@ -58,7 +114,7 @@ static void init_ulp_program()
 #define t1 ((uint16_t)ulp_t1)
 #define t2 ((int16_t) ulp_t2)
 #define t3 ((int16_t) ulp_t3)
-#define p1 ((uint16_t)ulp_p1)
+#define p1 ((int16_t)ulp_p1)
 #define p2 ((int16_t) ulp_p2)
 #define p3 ((int16_t) ulp_p3)
 #define p4 ((int16_t) ulp_p4)
@@ -68,9 +124,45 @@ static void init_ulp_program()
 #define p8 ((int16_t) ulp_p8)
 #define p9 ((int16_t) ulp_p9)
 
+#define temp_a ((uint8_t) ulp_temp_a)
+#define temp_b ((uint8_t) ulp_temp_b)
+#define temp_c ((uint8_t) ulp_temp_c)
+
+#define BME280_S32_t int32_t
+#define BME280_U32_t uint32_t
+#define BME280_S64_t int64_t
+
+static BME280_S32_t bme280_t_fine;
+
+static BME280_S32_t bme280_compensate_T(BME280_S32_t adc_T) {
+    BME280_S32_t var1, var2, T;
+    var1  = ((((adc_T>>3) - ((BME280_S32_t)t1<<1))) * ((BME280_S32_t)t2)) >> 11;
+    var2  = (((((adc_T>>4) - ((BME280_S32_t)t1)) * ((adc_T>>4) - ((BME280_S32_t)t1))) >> 12) *
+             ((BME280_S32_t)t3)) >> 14;
+    bme280_t_fine = var1 + var2;
+    T  = (bme280_t_fine * 5 + 128) >> 8;
+    return T;
+}
+
+
+
 
 static void print_status()
 {
+
+    // Must do Temp first since bme280_t_fine is used by the other compensation functions
+
+    BME280_S32_t temp ;
+    uint32_t adc_T = (uint32_t)(((temp_a << 16) | (temp_b << 8) | temp_c) >> 4);
+    if (adc_T == 0x80000 || adc_T == 0xfffff) {
+        temp = 0;
+    } else {
+        temp = bme280_compensate_T(adc_T);
+    }
+
+    printf("ADC_T: %i\n", adc_T);
+    printf("Temp: %i\n", temp);
+
     printf("%d ", t1);
     printf("%d ", t2);
     printf("%d \n", t3);
@@ -84,11 +176,15 @@ static void print_status()
     printf("%d ", p8);
     printf("%d ", p9);
     printf("\n");
+    printf("%d ", temp_a);
+    printf("%d ", temp_b);
+    printf("%d \n", temp_c);
 
 }
 
 void app_main()
 {
+
     esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
     if (cause != ESP_SLEEP_WAKEUP_ULP) {
         printf("Not ULP wakeup, initializing ULP\n");
@@ -102,9 +198,10 @@ void app_main()
 
     ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup() );
 
-    /* Start the program */
     esp_err_t err = ulp_run((&ulp_entry - RTC_SLOW_MEM) / sizeof(uint32_t));
     ESP_ERROR_CHECK(err);
+
+    vTaskDelay(10);
 
     esp_deep_sleep_start();
 
